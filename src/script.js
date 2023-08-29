75270cd3e4c4a1f5e021b8a3a063900faaa73ddb
Erandir Mota <erandirsm13@gmail.com>
2023-08-28 21:53:18 -0300

    finalized

--- src/script.js
+++ src/script.js
@@ -2,7 +2,37 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(numero1, numero2, operador) {
+  let resultado;
+  let num1 = parseInt(numero1);
+  let num2 = parseInt(numero2);
 
+  switch (operador) {
+    case "+":
+      resultado = num1 + num2;
+      break;
+    case "-":
+      resultado = num1 - num2;
+      break;
+    case "/":
+      resultado = num1 / num2;
+      break;
+    case "*":
+      resultado = num1 * num2;
+      break;
+    case "e":
+      resultado = num1 * num1;
+      for (let contador = 0; contador < resultado; contador++) {
+        resultado = resultado * num1;
+      }
+      break;
+  }
+
+  if (resultado === undefined || resultado > 1000000) 
+    resultado = "ERRO";
+
+    return resultado;
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
